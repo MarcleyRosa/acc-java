cca49618b93f29ac000285f9a4942dd278153a83
Marcley <marcleyrosa@gmail.com>
2023-04-29 14:14:05 -0300

    feat: exercise fatorial implemented

---
+++ essenciais-de-java/acc-java-010-exercises-fatorial/src/main/java/com/trybe/acc/java/Fatorial.java
@@ -0,0 +1,17 @@
+package com.trybe.acc.java;
+
+public class Fatorial {
+
+  /**
+   * Descrição da Class.
+   * 
+   * @param args fatorial de 18.
+   */
+  public static void main(String[] args) {
+    long fatorial18 = 6402373705728000L;
+
+    System.out.println("O fatorial de 18 é igual a " + fatorial18);
+
+  }
+
+}
