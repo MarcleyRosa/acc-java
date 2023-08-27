a3df183f7eb8de9ce181a52daf06c63d846c72f0
Marcley <marcleyrosa@gmail.com>
2023-08-27 13:51:54 -0300

    feat: class Pessoa

---
+++ Projetos/sistema-de-votacao/src/main/java/com/trybe/acc/java/sistemadevotacao/Pessoa.java
@@ -0,0 +1,13 @@
+package com.trybe.acc.java.sistemadevotacao;
+
+public abstract class Pessoa {
+  protected String nome;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+}
