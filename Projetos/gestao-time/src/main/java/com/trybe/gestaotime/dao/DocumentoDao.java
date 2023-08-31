9b0ec3852e5bdf831ed21b0192c46394218a5844
Marcley <marcleyrosa@gmail.com>
2023-08-31 20:28:18 -0300

    feat: dao DocumentoDao

---
+++ Projetos/gestao-time/src/main/java/com/trybe/gestaotime/dao/DocumentoDao.java
@@ -0,0 +1,11 @@
+package com.trybe.gestaotime.dao;
+
+import com.trybe.gestaotime.model.Documento;
+
+public class DocumentoDao extends GenericDao<Documento, Integer> {
+
+  public DocumentoDao() {
+    super(Documento.class);
+  }
+
+}
